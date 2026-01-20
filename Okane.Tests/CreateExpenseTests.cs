a7e84537a389993c86ffe0cba35b946a58c428a7
orodriguez <omarjavier15@gmail.com>
2026-01-19 21:13:52 -0400

    Add first test

---
+++ Okane.Tests/CreateExpenseTests.cs
@@ -0,0 +1,27 @@
+﻿namespace Okane.Tests;
+
+public class CreateExpenseTests
+{
+    [Fact]
+    public void Test()
+    {
+        var expense = CreateExpense(10, "Food");
+
+        Assert.Equal(10, expense.Amount);
+        Assert.Equal("Food", expense.CategoryName);
+    }
+
+    private Expense CreateExpense(int amount, string category)
+    {
+        var handler = new CreateExpenseHandler();
+        return handler.Exec(amount, category);
+    }
+}
+
+internal class CreateExpenseHandler
+{
+    public Expense Exec(int amount, string category) => 
+        new(amount, category);
+}
+
+public record Expense(int Amount,  string CategoryName);
